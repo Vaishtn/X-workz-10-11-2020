2158247f1b91148d2c7f1ccdced0e91603f7ffe3
Vaishtn <73684741+Vaishtn@users.noreply.github.com>
2020-11-10 16:26:17 +0530

    Add files via upload

---
+++ Calculator4.java
@@ -0,0 +1,30 @@
+class Calculator4{
+	public static int add(){
+	return 40+50;
+	}
+	
+	public static int division(){
+	return 40/50;
+	}
+	
+	public static int multiply(){
+	return 40*50;
+	}
+	
+	public static int subtract(){
+	return 40-50;
+	}
+	
+	public static void main(String args[]){
+	int sum=Calculator4.add();
+	System.out.println("The sum is:" +sum);
+	int div=Calculator4.division();
+	System.out.println("The div is:" +div);
+	int multi=Calculator4.multiply();
+	System.out.println("The multi is:" +multi);
+	int sub=Calculator4.subtract();
+	System.out.println("The sub is:" +sub);
+	
+	}
+}
+	
